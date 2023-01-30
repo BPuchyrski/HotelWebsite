98230978092588851b2f9a0ea5d822c05779074a
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-01-30 15:08:14 +0100

    u

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-const menuBtn = document.querySelector(".header-nav__btn");
-const menu = document.querySelector(".header-nav__list");
-
-menuBtn.addEventListener("click", () => {
-  menu.classList.toggle("show-menu");
+const menuBtn = document.querySelectorAll(".header-nav__btn");
+const menu = document.querySelector(".header-nav");
+// const hideMenuBtn = document.querySelector(".header-nav__btn--hide");
+const bla = document.querySelectorAll("div");
+menuBtn.forEach((element) => {
+  element.addEventListener("click", () => {
+    menu.classList.toggle("show-menu");
+  });
 });
