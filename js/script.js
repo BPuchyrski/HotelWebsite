7fe4bd44640ee02c12f8fef46b3a233cce52d88d
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-02-01 12:11:19 +0100

    u

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const menuBtn = document.querySelectorAll(".header-nav__btn");
 const menu = document.querySelector(".header-nav");
-// const hideMenuBtn = document.querySelector(".header-nav__btn--hide");
 const bla = document.querySelectorAll("div");
 menuBtn.forEach((element) => {
   element.addEventListener("click", () => {
